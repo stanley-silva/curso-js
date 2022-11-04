dd8fa7e36697ae19de8e8c4dbb39527fd090bc25
Stanley Silva <contact@stanleysilva.com.br>
2022-11-03 22:42:12 -0400

    update

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+vel = 58
+console.log(`A velocidade do seu carro é ${vel}km por hora`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+país = 'Brasil'
+console.log (`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Brasileiro!')
+}   else {
+    console.log ('Você é estrangeiro!')
+}
