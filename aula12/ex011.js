e25e982748171310994c7018715f741da975966f
Stanley Silva <contact@stanleysilva.com.br>
2022-11-07 00:48:46 -0400

    update

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,9 @@
-idade = 15
-if (idade < 18) {
-    console.log(`Menor de idade`)
-} else {
-    console.log(`Maior de idade`)
+idade = 69
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log(`Não vota`)
+} else if (idade < 18 || idade > 65) {
+    console.log ('Voto opcional')
+}   else { 
+    console.log('Voto obrigatório!')
 }
